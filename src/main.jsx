a0cd6fd5547ac922c991fbd1070cf3eec45d3d16
BARRIOS <iml.health@pm.me>
2026-08-10 09:57:49 +0200

    Load simplified site copy and static project image styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./siteCopyUpdate.js";
 import App from "./App.jsx";
 import "./styles/global.css";
+import "./styles/sitePolish.css";
 
 createRoot(document.getElementById("root")).render(<App />);
